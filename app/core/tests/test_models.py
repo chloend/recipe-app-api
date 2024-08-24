691edea0d72833341337c716791419e21cd04a3f
Chloé ERNDT <chloerndt@gmail.com>
2024-08-24 13:36:40 +0200

    Added 'test_create_recipe' test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Tests for models
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -47,3 +51,24 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+
+            price=Decimal('5.50'),
+            # Note : it's best practice to use Integer instead of Decimal
+            # to store prices especially when performing lots of
+            # calculations on float numbers.
+
+            description='Sample recipe description.',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
